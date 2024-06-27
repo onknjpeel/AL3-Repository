416bfd1f6931f5fbd6b4cf6b5ebafa90bf88ae11
onknjpeel <k023g0103@g.neec.ac.jp>
2024-06-27 19:31:07 +0900

    進捗保存

---
+++ DirectXGame/Enemy.h
@@ -0,0 +1,19 @@
+#include "WorldTransform.h"
+#include "Model.h"
+#include "ViewProjection.h"
+
+class Enemy {
+	private:
+		Enemy();
+
+		~Enemy();
+
+		void Initialize();
+
+		void Update();
+
+		void Draw();
+
+	public:
+
+};
